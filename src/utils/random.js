8ceb0623d4dc3a8584a2db2cd6d681ed19b52fae
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-09-18 11:22:52 +0100

    Initial commit

---
+++ src/utils/random.js
@@ -0,0 +1,7 @@
+function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+export function getRandomEntry(array) {
+  return array[getRandomInt(0, array.length - 1)];
+}
